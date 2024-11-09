0d8e877dcdefdd564921743f71c8a59eb0f93770
Alvarez-T <thiago.alvarez2012@gmail.com>
2024-11-09 19:50:17 -0300

    Correção git e ajustes html

---
+++ src/BetterPrice/Areas/Conta/Pages/Cadastro.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BetterPrice.Areas.Conta.Pages
+{
+    public class CadastroModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
